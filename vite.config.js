4d7d6a44bc6d1425d9961117ed1ab44df25ab2c3
Kai Legum <kdlegum@gmail.com>
2026-03-08 15:05:49 +0000

    Vite config path

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  base: process.env.BASE_PATH || '/',
   plugins: [svelte()]
 });
